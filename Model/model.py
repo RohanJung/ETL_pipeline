6c6fa9f365d88874a8d44490aa6f2078a8a15ab9
RohanJung <rohanjungtmg@gmail.com>
2025-07-24 22:44:00 +0545

    cleaned poulation and projects data

--- Model/model.py
+++ Model/model.py
@@ -1,1 +1,13 @@
- e 
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split 
+
+df_projects = pd.read_csv('C:/Users/rest/Documents/python/etl/ETL_pipeline/Data/projects_data.csv',dtype=str)
+
+df_projects=df_projects.isnull().sum()
+
+print(df_projects)
+
+
+df_population = pd.read_csv('C:/Users/rest/Documents/python/etl/ETL_pipeline/Data/population_data.csv',skiprows=4)
+print(df_population)
